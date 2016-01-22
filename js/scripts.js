2320300c9ff6cce321137d1f7ecc0b3ba87fd031
josh overly <joshoverly@yahoo.com>
2016-01-22 09:09:19 -0800

    changes numbers divisible by 3 to ' ping'

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,12 @@ var output = [];
     if(i % 5 === 0) {
       output.push("pong");
     } else {
+      if(i % 3 === 0) {
+      output.push("ping");
+      } else {
       output.push(i);
     }
+  }
   }
   };
 console.log(output);
--- spec/specs.js
+++ spec/specs.js
@@ -1,12 +1,15 @@
 describe('pingPong', function(num) {
   it("adds a string of numbers from 1 to user input", function() {
-    expect(pingPong(3)).to.eql([1, 2, 3]);
+    expect(pingPong(3)).to.eql([1, 2, "ping"]);
   });
   it("returns 'ping pong' if a number is divisible by fifteen", function() {
-    expect(pingPong(15)).to.eql([1, 2, 3, 4, "pong", 6, 7, 8, 9, "pong", 11, 12, 13, 14, "ping pong"]);
+    expect(pingPong(15)).to.eql([1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", "pong", 11, "ping", 13, 14, "ping pong"]);
   });
   it("returns 'pong' if a number is divisible by five", function() {
-    expect(pingPong(5)).to.eql([1, 2, 3, 4, "pong"]);
+    expect(pingPong(5)).to.eql([1, 2, "ping", 4, "pong"]);
+  });
+  it("returns 'ping' if a number is divisible by three", function() {
+    expect(pingPong(3)).to.eql([1, 2, "ping"]);
   });
 
 });
